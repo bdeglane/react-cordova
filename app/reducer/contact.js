0ac76d32532f526bc07665f705789a9ffa2bf7e9
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-07-27 12:04:21 +0200

    add redirect add remove

--- app/reducer/contact.js
+++ app/reducer/contact.js
@@ -1,4 +1,5 @@
 import * as actions from '../constant/contact';
+import {browserHistory} from 'react-router';
 
 const initialState = {
 	contacts: []
@@ -13,10 +14,11 @@ export const contact = (state = initialState, action) => {
 	switch (action.type) {
 		case actions.ADD_CONTACT:
 			state.contacts.push(action.param);
+			browserHistory.push('/list');
 			return state;
 			break;
 		case actions.REMOVE_CONTACT:
-			let contact = state.contacts.find(action.param);
+			let contact = state.contacts.indexOf(action.param);
 			if (contact != 'undefined')
 				state.contacts.splice(contact, 1);
 			return state;
